6ae3774e60b8f86988ab4c491a6edec11eb1609c
Salvatore Capano <capano_salvatore@libero.it>
2023-01-09 13:39:48 +0100

    -Inizializzazione progetto -Aggiunto ciclo while che itera gli elementi della lista

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/* 
+    Definizione e inizializzazione lista della spesa
+    Ciclo While: 
+        Creazione nuovo elemento li
+        Inserimento testo nel li
+        Inserire il li nella lista
+*/
+
+// Lista
+const groceryList = ['Pane', 'Pasta', 'Olio', 'Uova', 'Banane', 'Latte', 'Carote'];
+
+// Variabile Contatore
+let i = 0;
+
+do {
+
+    console.log(i);
+
+    i++;
+
+} while (i < groceryList.length)
